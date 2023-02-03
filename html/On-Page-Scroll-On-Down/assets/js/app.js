a5cb44c21131e67d48e706cb7a07fc4a2cf042d0
danghung <hungdang02042003@gmail.com>
2023-02-03 10:48:52 +0700

    feat: on page scroll on down

--- html/On-Page-Scroll-On-Down/assets/js/app.js
+++ html/On-Page-Scroll-On-Down/assets/js/app.js
@@ -2,9 +2,7 @@ const reveal = () => {
 	const reveals = document.querySelectorAll('.reveal');
 	for (let i = 0; i < reveals.length; i++) {
 		let windowHeight = window.innerHeight;
-		console.log('🚀 ~ file: app.js:5 ~ reveal ~ windowHeight', windowHeight);
 		let revealTop = reveals[i].getBoundingClientRect().top;
-		console.log('🚀 ~ file: app.js:7 ~ reveal ~ revealTop', revealTop);
 		let revealPoint = 150;
 		if (revealTop < windowHeight - revealPoint) {
 			reveals[i].classList.add('active');
