fec43501d62bf172b54f257e7dfa70bcac4ec7b0
danghung <hungdang02042003@gmail.com>
2023-02-08 13:08:53 +0700

    kết nối & lấy toàn bộ dự liệu trong db

--- firebase/lear-firebase/src/pages/HomePage.jsx
+++ firebase/lear-firebase/src/pages/HomePage.jsx
@@ -1,7 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { collection, getDocs } from 'firebase/firestore';
+
+import { db } from '../firebase/config-firebse';
 
 const HomePage = () => {
-	return <div>HomePage</div>;
+	const [posts, setPosts] = useState([]);
+	useEffect(() => {
+		const colRef = collection(db, 'posts'); // collection -> dùng để kết nối với db
+		getDocs(colRef).then((snapshot) => {
+			// getDocs -> lấy ra toàn bộ dữ liệu trong firebase
+			const posts = [];
+			snapshot.docs.forEach((doc) => {
+				posts.push({
+					id: doc.id,
+					...doc.data(),
+				});
+			});
+			setPosts(posts);
+		});
+	}, []);
+	return (
+		<div>
+			{posts &&
+				posts.length > 0 &&
+				posts.map((post) => (
+					<div key={post.id}>
+						<h2>{post.title}</h2>
+					</div>
+				))}
+		</div>
+	);
 };
 
 export default HomePage;
