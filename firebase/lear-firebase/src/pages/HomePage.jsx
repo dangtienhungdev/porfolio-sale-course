28296b6daa630a4a43e768a9b2c63c7d1c063f89
danghung <hungdang02042003@gmail.com>
2023-02-08 13:24:03 +0700

    thêm 1 bản ghi mới vào db

--- firebase/lear-firebase/src/pages/HomePage.jsx
+++ firebase/lear-firebase/src/pages/HomePage.jsx
@@ -1,10 +1,12 @@
 import React, { useEffect, useState } from 'react';
-import { collection, getDocs } from 'firebase/firestore';
+import { addDoc, collection, getDocs } from 'firebase/firestore';
 
 import { db } from '../firebase/config-firebse';
 
 const HomePage = () => {
 	const [posts, setPosts] = useState([]);
+	const [title, setTitle] = useState('');
+	const [author, setAuthor] = useState('');
 	useEffect(() => {
 		const colRef = collection(db, 'posts'); // collection -> dùng để kết nối với db
 		getDocs(colRef).then((snapshot) => {
@@ -19,16 +21,50 @@ const HomePage = () => {
 			setPosts(posts);
 		});
 	}, []);
+	const handleAddPost = (e) => {
+		e.preventDefault();
+		console.log('submit');
+		const colRef = collection(db, 'posts'); // collection -> dùng để kết nối với db
+		addDoc(colRef, {
+			// addDoc -> thêm 1 dữ liệu vào bản ghi
+			title,
+			author,
+		})
+			.then(() => console.log('success'))
+			.catch(() => console.log('error'));
+	};
 	return (
-		<div>
-			{posts &&
-				posts.length > 0 &&
-				posts.map((post) => (
-					<div key={post.id}>
-						<h2>{post.title}</h2>
-					</div>
-				))}
-		</div>
+		<>
+			<div className="grid grid-cols-4 gap-4 m-4">
+				{posts &&
+					posts.length > 0 &&
+					posts.map((post) => (
+						<div key={post.id} className="p-4 bg-gray-300">
+							<h2>{post.title}</h2>
+							<h3>{post.author}</h3>
+						</div>
+					))}
+			</div>
+			<div className="mx-auto w-full max-w-lg">
+				<form className="w-full" onSubmit={handleAddPost}>
+					<input
+						type="text"
+						className="p-2 border border-gray-500 w-full outline-none"
+						placeholder="title"
+						value={title}
+						onChange={(e) => setTitle(e.target.value)}
+					/>
+					<input
+						type="text"
+						className="p-2 border border-gray-500 w-full outline-none"
+						placeholder="author"
+						value={author}
+						onChange={(e) => setAuthor(e.target.value)}
+					/>
+					<button className="mt-4 w-full p-2 bg-green-400">submit</button>
+				</form>
+			</div>
+		</>
 	);
 };
 
