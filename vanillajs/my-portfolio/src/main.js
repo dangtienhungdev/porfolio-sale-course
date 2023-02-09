3e73000425924ebf013ff9ab3c6ab749c964c079
danghung <hungdang02042003@gmail.com>
2023-02-09 23:14:00 +0700

    initial

---
+++ vanillajs/my-portfolio/src/main.js
@@ -0,0 +1,7 @@
+import './style.scss';
+
+document.querySelector('#app').innerHTML = `
+  <div class='underline'>
+    <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae cum, atque alias vero, doloribus, expedita eius at nulla blanditiis provident dolores vel fugit iure voluptatum minima ipsum a officiis velit?</h2>
+  </div>
+`;
