78a7afc1579affdaba236ef753ef1e1fc97c3cfb
danghung <hungdang02042003@gmail.com>
2023-02-24 16:38:12 +0700

    connection pools

--- nodejs/nodejs-udemy-hoi-dan-IT/chapter2/src/configs/database.js
+++ nodejs/nodejs-udemy-hoi-dan-IT/chapter2/src/configs/database.js
@@ -2,12 +2,24 @@ require('dotenv').config();
 const mysql = require('mysql2');
 
 // create the connection to database
-const connection = mysql.createConnection({
+// const connection = mysql.createConnection({
+// 	host: process.env.DB_HOST,
+// 	user: process.env.DB_USER,
+// 	password: process.env.DB_PASSWORD,
+// 	port: process.env.DB_PORT, // defautl port: 3306
+// 	database: process.env.DB_NAME,
+// });
+
+// connection pools
+const connection = mysql.createPool({
 	host: process.env.DB_HOST,
 	user: process.env.DB_USER,
 	password: process.env.DB_PASSWORD,
 	port: process.env.DB_PORT, // defautl port: 3306
 	database: process.env.DB_NAME,
+	waitForConnections: true,
+	connectionLimit: 10,
+	queueLimit: 0,
 });
 
 module.exports = connection;
