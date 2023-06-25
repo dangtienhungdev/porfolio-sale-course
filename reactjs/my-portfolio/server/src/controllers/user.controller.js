bff16b1b0c9b172977186437b4978c1d0b11fbc6
danghung <hungdang02042003@gmail.com>
2023-06-26 00:02:09 +0700

    get all users

--- reactjs/my-portfolio/server/src/controllers/user.controller.js
+++ reactjs/my-portfolio/server/src/controllers/user.controller.js
@@ -1,3 +1,13 @@
+import User from '../models/user.model.js';
+
 export const userController = {
-  /*  */
+  /* getAll */
+  getAll: async (_, res) => {
+    try {
+      const users = await User.find();
+      return res.status(200).json({ data: users });
+    } catch (error) {
+      return res.status(500).json({ message: error.message });
+    }
+  },
 };
--- reactjs/my-portfolio/server/src/routes/index.js
+++ reactjs/my-portfolio/server/src/routes/index.js
@@ -1,9 +1,10 @@
 import authRoutes from './auth.routes.js';
 import express from 'express';
+import userRoutes from './user.routes.js';
 
 const router = express.Router();
 
-const rootRoutes = [authRoutes];
+const rootRoutes = [authRoutes, userRoutes];
 
 rootRoutes.forEach((route) => {
   router.use(route);
