8311580d5cb702a189f5b0756d2e6d7f44ab973e
dangtienhung <danghung02042003@gmail.com>
2024-04-28 13:11:50 +0700

    initial

---
+++ nextjs/next-mmo/client/src/app/(auth)/login/layout.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const LoginLayout = ({
+	children,
+}: Readonly<{
+	children?: React.ReactNode;
+}>) => {
+	return (
+		<div>
+			ahihiih
+			{children}
+		</div>
+	);
+};
+
+export default LoginLayout;
