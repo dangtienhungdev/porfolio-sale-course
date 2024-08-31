792fb50d4801787e950ff2d5b7fa323fb31231a7
dangtienhung <danghung02042003@gmail.com>
2024-08-31 19:01:19 +0700

    chương 18: chap 22: set protected-router & rejected-router

--- reactjs/shopee/src/useRouterElement.tsx
+++ reactjs/shopee/src/useRouterElement.tsx
@@ -1,14 +1,27 @@
-import Login from './pages/login'
+import { Navigate, Outlet, useRoutes } from 'react-router-dom'
+
 import MainLayout from './layouts/main-layout'
-import ProductList from './pages/porduct-list'
-import Register from './pages/register'
 import RegisterLayout from './layouts/register-layout'
-import { useRoutes } from 'react-router-dom'
+import Login from './pages/login'
+import ProductList from './pages/porduct-list'
+import ProfilePage from './pages/profile'
+import Register from './pages/register'
+
+const isAuthenticated = false
+
+const ProtectedRoute = () => {
+  return isAuthenticated ? <Outlet /> : <Navigate to='/login' />
+}
+
+const RejectedRoute = () => {
+  return !isAuthenticated ? <Outlet /> : <Navigate to='/' />
+}
 
 export default function useRouterElement() {
   const routerElement = useRoutes([
     {
       path: '/',
+      index: true,
       element: (
         <MainLayout>
           <ProductList />
@@ -16,20 +29,40 @@ export default function useRouterElement() {
       )
     },
     {
-      path: '/login',
-      element: (
-        <RegisterLayout>
-          <Login />
-        </RegisterLayout>
-      )
+      path: '',
+      element: <ProtectedRoute />,
+      children: [
+        {
+          path: '/profile',
+          element: (
+            <MainLayout>
+              <ProfilePage />
+            </MainLayout>
+          )
+        }
+      ]
     },
     {
-      path: '/register',
-      element: (
-        <RegisterLayout>
-          <Register />
-        </RegisterLayout>
-      )
+      path: '',
+      element: <RejectedRoute />,
+      children: [
+        {
+          path: '/login',
+          element: (
+            <RegisterLayout>
+              <Login />
+            </RegisterLayout>
+          )
+        },
+        {
+          path: '/register',
+          element: (
+            <RegisterLayout>
+              <Register />
+            </RegisterLayout>
+          )
+        }
+      ]
     }
   ])
 
