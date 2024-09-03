dcbf59071692c5ce1456bf6dc22b29e32dde1726
dangtienhung <danghung02042003@gmail.com>
2024-09-03 09:58:06 +0700

    chương 19: chap 1: code ui sort product list

--- reactjs/shopee/src/pages/porduct-list/sort-product-list/sort-product-list.tsx
+++ reactjs/shopee/src/pages/porduct-list/sort-product-list/sort-product-list.tsx
@@ -1,5 +1,137 @@
+import { Link } from 'react-router-dom'
+import classNames from 'classnames'
+import path from '../../../constants/path'
+
 const SortProductList = () => {
-  return <div className=''>sort filer</div>
+  const isActiveSortBy = (sortBy: string) => {
+    return sortBy === 'view'
+  }
+
+  const page = 1
+  const pageSize = 10
+
+  return (
+    <div className='bg-gray-300/40 py-4 px-3'>
+      <div className='flex flex-wrap items-center justify-between gap-2'>
+        <div className='flex flex-wrap items-center gap-2'>
+          <div>Sắp xếp theo</div>
+          <button
+            className={classNames('h-8 px-4 text-center text-sm capitalize ', {
+              'bg-orange text-white hover:bg-orange/80': isActiveSortBy('view'),
+              'bg-white text-black hover:bg-slate-100': !isActiveSortBy('view')
+            })}
+          >
+            Phổ biến
+          </button>
+          <button
+            className={classNames('h-8 px-4 text-center text-sm capitalize ', {
+              'bg-orange text-white hover:bg-orange/80': isActiveSortBy('createdAt'),
+              'bg-white text-black hover:bg-slate-100': !isActiveSortBy('createdAt')
+            })}
+          >
+            Mới nhất
+          </button>
+          <button
+            className={classNames('h-8 px-4 text-center text-sm capitalize ', {
+              'bg-orange text-white hover:bg-orange/80': isActiveSortBy('sold'),
+              'bg-white text-black hover:bg-slate-100': !isActiveSortBy('sold')
+            })}
+          >
+            Bán chạy
+          </button>
+          <select
+            className={classNames('h-8  px-4 text-left text-sm capitalize  outline-none ', {
+              'bg-orange text-white hover:bg-orange/80': isActiveSortBy('price'),
+              'bg-white text-black hover:bg-slate-100': !isActiveSortBy('price')
+            })}
+          >
+            <option value='' disabled className='bg-white text-black'>
+              Giá
+            </option>
+            <option value={'asc'} className='bg-white text-black'>
+              Giá: Thấp đến cao
+            </option>
+            <option value={'desc'} className='bg-white text-black'>
+              Giá: Cao đến thấp
+            </option>
+          </select>
+        </div>
+
+        <div className='flex items-center'>
+          <div>
+            <span className='text-orange'>{page}</span>
+            <span>/{pageSize}</span>
+          </div>
+          <div className='ml-2 flex'>
+            {page === 1 ? (
+              <span className='flex h-8 w-9 cursor-not-allowed items-center justify-center rounded-tl-sm rounded-bl-sm bg-white/60  shadow hover:bg-slate-100'>
+                <svg
+                  xmlns='http://www.w3.org/2000/svg'
+                  fill='none'
+                  viewBox='0 0 24 24'
+                  strokeWidth={1.5}
+                  stroke='currentColor'
+                  className='h-3 w-3'
+                >
+                  <path strokeLinecap='round' strokeLinejoin='round' d='M15.75 19.5L8.25 12l7.5-7.5' />
+                </svg>
+              </span>
+            ) : (
+              <Link
+                to={{
+                  pathname: ''
+                }}
+                className='flex h-8 w-9  items-center justify-center rounded-tl-sm rounded-bl-sm bg-white  shadow hover:bg-slate-100'
+              >
+                <svg
+                  xmlns='http://www.w3.org/2000/svg'
+                  fill='none'
+                  viewBox='0 0 24 24'
+                  strokeWidth={1.5}
+                  stroke='currentColor'
+                  className='h-3 w-3'
+                >
+                  <path strokeLinecap='round' strokeLinejoin='round' d='M15.75 19.5L8.25 12l7.5-7.5' />
+                </svg>
+              </Link>
+            )}
+            {false ? (
+              <span className='flex h-8 w-9 cursor-not-allowed items-center justify-center rounded-tl-sm rounded-bl-sm bg-white/60  shadow hover:bg-slate-100'>
+                <svg
+                  xmlns='http://www.w3.org/2000/svg'
+                  fill='none'
+                  viewBox='0 0 24 24'
+                  strokeWidth={1.5}
+                  stroke='currentColor'
+                  className='h-3 w-3'
+                >
+                  <path strokeLinecap='round' strokeLinejoin='round' d='M8.25 4.5l7.5 7.5-7.5 7.5' />
+                </svg>
+              </span>
+            ) : (
+              <Link
+                to={{
+                  pathname: path.home
+                }}
+                className='flex h-8 w-9  items-center justify-center rounded-tl-sm rounded-bl-sm bg-white  shadow hover:bg-slate-100'
+              >
+                <svg
+                  xmlns='http://www.w3.org/2000/svg'
+                  fill='none'
+                  viewBox='0 0 24 24'
+                  strokeWidth={1.5}
+                  stroke='currentColor'
+                  className='h-3 w-3'
+                >
+                  <path strokeLinecap='round' strokeLinejoin='round' d='M8.25 4.5l7.5 7.5-7.5 7.5' />
+                </svg>
+              </Link>
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default SortProductList
